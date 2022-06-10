dab3080d25140715b4cbdbc7b2994446e539f578
Enric-Tobena <74415269+Enric-Tobena@users.noreply.github.com>
2022-06-10 14:28:52 +0200

    Add files via upload

--- ShipDetectorTest.py
+++ ShipDetectorTest.py
@@ -11,95 +11,93 @@ class MyTestCase(unittest.TestCase):
                  [244, 71, 21],
                  [226, 229, 223]]
         your_solution = shipDetectorIterativo(image)
-        solution = [[1, 0]]
+        solution = [[0, 1], [1, 0], [2, 1]]
 
         self.assertListEqual(your_solution, solution)
 
     def test_practice_validator_2_x_2_iterativo(self):
-        image = image_generator(2)
-        your_solution = shipDetectorIterativo(image['image'])
+        image = [[3, 6],
+                 [2, 11]]
+        your_solution = shipDetectorIterativo(image)
+        solution = [[1, 1]]
 
-        self.assertListEqual(your_solution, image['solution'])
+        self.assertListEqual(your_solution, solution)
 
     def test_practice_validator_3_x_3_iterativo(self):
-        image = image_generator(3)
-        your_solution = shipDetectorIterativo(image['image'])
-        print(image['image'])
-        print(your_solution)
-        print(image['solution'])
+        image = [[24, 54, 32],
+                 [12, 25, 18],
+                 [30, 40, 11]]
+        your_solution = shipDetectorIterativo(image)
+        solution = [[0, 1], [2, 1]]
 
-        self.assertListEqual(your_solution, image['solution'])
+        self.assertListEqual(your_solution, solution)
 
     def test_practice_validator_4_x_4_iterativo(self):
-        image = image_generator(4)
-        your_solution = shipDetectorIterativo(image['image'])
-        print(image['image'])
-        print(your_solution)
-        print(image['solution'])
-        self.assertListEqual(your_solution, image['solution'])
+        image = [[15, 12, 25, 9],
+                 [24, 32, 12, 4],
+                 [18, 42, 35, 52],
+                 [30, 68, 112, 39]]
+        your_solution = shipDetectorIterativo(image)
+        solution = [[0, 2], [2, 3], [3, 2]]
+
+        self.assertListEqual(your_solution, solution)
 
     def test_practice_validator_5_x_5_iterativo(self):
-        image = image_generator(5)
-        your_solution = shipDetectorIterativo(image['image'])
-        print(image['image'])
-        print(your_solution)
-        print(image['solution'])
-        self.assertListEqual(your_solution, image['solution'])
+        image = [[15, 13, 40, 25, 41],
+                 [32, 18, 36, 27, 19],
+                 [10, 8, 38, 52, 116],
+                 [60, 34, 16, 38, 55],
+                 [39, 70, 17, 54, 33]]
+        your_solution = shipDetectorIterativo(image)
+        solution = [[0, 2], [0, 4], [1, 0], [2, 4], [3, 0], [4, 1], [4, 3]]
 
-    def test_practice_validator_10_x_10_iterativo(self):
-        image = image_generator(10)
-        your_solution = shipDetectorIterativo(image['image'])
-        print(image['image'])
-        print(your_solution)
-        print(image['solution'])
-        self.assertListEqual(your_solution, image['solution'])
-
-    def test_practice_validator_n_x_n_iterativo(self):
-        image = image_generator(random.randint(10, 100))
-        your_solution = shipDetectorIterativo(image['image'])
-        print(image['image'])
-        print(your_solution)
-        print(image['solution'])
-        self.assertListEqual(your_solution, image['solution'])
+        self.assertListEqual(your_solution, solution)
 
     def test_practice_validator_2_x_2_recursivo(self):
-        image = image_generator(2)
-        your_solution = shipDetectorRecursivo(image['image'])
+        image = [[3, 6],
+                 [2, 11]]
+        your_solution = shipDetectorRecursivo(image)
+        solution = [[1, 1]]
 
-        self.assertListEqual(your_solution, image['solution'])
+        self.assertListEqual(your_solution, solution)
 
     def test_practice_validator_3_x_3_recursivo(self):
-        image = image_generator(3)
-        your_solution = shipDetectorRecursivo(image['image'])
-        print(image['image'])
-        print(your_solution)
-        print(image['solution'])
+        image = [[24, 54, 32],
+                 [12, 25, 18],
+                 [30, 40, 11]]
+        your_solution = shipDetectorIterativo(image)
+        solution = [[0, 1], [2, 1]]
 
-        self.assertListEqual(your_solution, image['solution'])
+        self.assertListEqual(your_solution, solution)
 
     def test_practice_validator_4_x_4_recursivo(self):
-        image = image_generator(4)
-        your_solution = shipDetectorRecursivo(image['image'])
-        print(image['image'])
-        print(your_solution)
-        print(image['solution'])
-        self.assertListEqual(your_solution, image['solution'])
+        image = [[15, 12, 25, 9],
+                 [24, 32, 12, 4],
+                 [18, 42, 35, 52],
+                 [30, 68, 112, 39]]
+        your_solution = shipDetectorIterativo(image)
+        solution = [[0, 2], [2, 3], [3, 2]]
+
+        self.assertListEqual(your_solution, solution)
 
     def test_practice_validator_5_x_5_recursivo(self):
-        image = image_generator(5)
-        your_solution = shipDetectorRecursivo(image['image'])
-        print(image['image'])
-        print(your_solution)
-        print(image['solution'])
-        self.assertListEqual(your_solution, image['solution'])
+        image = [[15, 13, 40, 25, 41],
+                 [32, 18, 36, 27, 19],
+                 [10, 8, 38, 52, 116],
+                 [60, 34, 16, 38, 55],
+                 [39, 70, 17, 54, 33]]
+        your_solution = shipDetectorIterativo(image)
+        solution = [[0, 2], [0, 4], [1, 0], [2, 4], [3, 0], [4, 1], [4, 3]]
 
-    def test_practice_validator_10_x_10_recursivo(self):
+        self.assertListEqual(your_solution, solution)
+
+    def test_practice_validator_10_x_10(self):
         image = image_generator(10)
-        your_solution = shipDetectorRecursivo(image['image'])
-        print(image['image'])
-        print(your_solution)
-        print(image['solution'])
-        self.assertListEqual(your_solution, image['solution'])
+        iter_solution = shipDetectorIterativo(image['image'])
+        rec_solution = shipDetectorRecursivo(image['image'])
+
+        self.assertListEqual(iter_solution, rec_solution)
+
 
 if __name__ == '__main__':
     unittest.main()
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from findpeaks import findpeaks
 import random
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def image_generator(n_size=10):
@@ -21,7 +22,7 @@ def shipDetectorIterativo(image):
     positions = []
     for i in range(1, len(imageToAnalyze) - 1):
         for j in range(1, len(imageToAnalyze) - 1):
-            if smallerSides(i, j, imageToAnalyze) and smallerDiagonals(i, j, imageToAnalyze):
+            if smallerSides(i, j, imageToAnalyze):
                 positions.append([i - 1, j - 1])
 
     return positions
@@ -40,9 +41,9 @@ def analizarEntornoRecursivo(imageToAnalyze, positions, i, j):
     if j == len(imageToAnalyze) - 1:
         return analizarEntornoRecursivo(imageToAnalyze, positions, i + 1, 1)
 
-    if smallerSides(i, j, imageToAnalyze) and smallerDiagonals(i, j, imageToAnalyze):
+    if smallerSides(i, j, imageToAnalyze):
         positions.append([i - 1, j - 1])
-        
+
     return analizarEntornoRecursivo(imageToAnalyze, positions, i, j + 1)
 
 
@@ -61,11 +62,45 @@ def smallerSides(i, j, image):
     return num >= image[i + 1][j] and num >= image[i - 1][j] and num >= image[i][j + 1] and num >= image[i][j - 1]
 
 
-def smallerDiagonals(i, j, image):
-    num = image[i][j]
-    return num >= image[i + 1][j + 1] and num >= image[i - 1][j - 1] and num >= image[i - 1][j + 1] and num >= image[i + 1][j - 1]
-
 # TODO: En este programa, debereis generar las imagenes de costos correspondientes tal y como hicimos en laboratorios y en la práctica 1.
+
+def calcularTiempoIterativo():
+    import timeit
+    times = []
+    for x in range(4, 200, 4):
+        print("n =", x)
+        provImg = image_generator(x)['image']
+        times.append((x, timeit.timeit("shipDetectorIterativo(" + str(provImg) + ")",
+                                       setup="from __main__ import shipDetectorIterativo", number=100)))
+    return times
+
+
+def calcularTiempoRecursivo():
+    import timeit
+    times = []
+    for x in range(2, 32, 1):
+        print("n =", x)
+        provImg = image_generator(x)['image']
+        times.append((x, timeit.timeit("shipDetectorRecursivo(" + str(provImg) + ")",
+                                       setup="from __main__ import shipDetectorRecursivo", number=100)))
+    return times
+
+
+def setupGraphs(x_list, y_list):
+    print(x_list)
+    print(y_list)
+    plt.scatter(x_list, y_list)
+    plt.show()
+
+
+def empiricalTimes():
+    iterativeTime = calcularTiempoIterativo()
+    setupGraphs(*map(list, zip(*iterativeTime)))
+
+    recursiveTime = calcularTiempoRecursivo()
+    setupGraphs(*map(list, zip(*recursiveTime)))
+
+    return 0
 
 
 # Programa principal para la generación de las matrices 2D y la identificación de barcos.
@@ -75,5 +110,6 @@ if __name__ == '__main__':
         sys.exit('Usage: ' + sys.argv[0] + ' <matrix_size_number>')
 
     image2D = image_generator(int(sys.argv[1]))
-    imageWithBorders = setupBorders(image2D['image'])
-    print(imageWithBorders)
+    print(shipDetectorIterativo(image2D['image']))
+
+    empiricalTimes()
